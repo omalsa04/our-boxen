c292e5442fb0781e8256b3e3b5a828d4b06bc1c9
Samuel O'Malley <omalsa04@gmail.com>
2015-07-18 17:39:38 +0930

    Fixed params error

--- modules/people/manifests/omalsa04/params.pp
+++ modules/people/manifests/omalsa04/params.pp
@@ -1,8 +1,4 @@
-class people::omalsa04::params(
-  $my_sourcedir = $people::omalsa04::params::my_sourcedir,
-  $my_homedir   = $people::omalsa04::params::my_homedir,
-  $my_username  = $people::omalsa04::params::my_username
-) {
+class people::omalsa04::params {
   # $::luser and $::boxen_srcdir come from Boxen's custom facts
   $my_username  = $::luser
   $my_homedir   = "/Users/${my_username}"
